8a55fe0cff0aaf5bf55a29dc6c22457e6f48e597
Nejtrino0 <48865191+Nejtrino0@users.noreply.github.com>
2021-02-14 22:08:59 +0400

    d

--- test_npm_page.py
+++ test_npm_page.py
@@ -6,22 +6,20 @@ from base.npm_page import NPMPage
 
 
 @pytest.mark.npm
-@pytest.mark.parametrize('search', [
-    ('react',)
-    # ('vue',)
+@pytest.mark.parametrize('search', ['react', 'vue'
 ])
 @pytest.mark.parametrize('page', [NPMPage], indirect=['page', ])
 class TestNPMPage:
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_search_positive(self, page, search):
         page.fill_search_input(search[0])
         page.click_search_button()
         assert page.get_result_list(search[0]), \
             f'search for "{search[0]}" not gave any results'
 
-    @pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
-    def test_search_negative(self, page, search):
-        page.fill_search_input(search[0])
-        page.click_search_button()
-        assert not page.get_result_list('angular'), \
-            f'search for "{search[0]}" not gave any results'
+    # @pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
+    # def test_search_negative(self, page, search):
+    #     page.fill_search_input(search[0])
+    #     page.click_search_button()
+    #     assert not page.get_result_list('angular'), \
+    #         f'search for "{search[0]}" not gave any results'
